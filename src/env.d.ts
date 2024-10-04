3d81b8b186d5f3557344ef4cd716ca03183d70b7
ff <frend.fernando@LIVE.COM>
2024-10-04 08:23:28 -0700

    init csapf

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
+
+declare namespace App {
+    interface Locals {}
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const flowbite = require('flowbite/plugin')
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
+
+export default {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+		, './node_modules/flowbite/**/*.js'
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [ flowbite({ charts: false }), addDynamicIconSelectors() ],
+}
