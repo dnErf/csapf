4677627b81283b2ecf92191c44c2524ceabe7751
ff <frend.fernando@LIVE.COM>
2024-10-12 10:40:31 -0700

    ft init lucia | noble hash

--- src/data/database.ts
+++ src/data/database.ts
@@ -1,9 +1,11 @@
 import type { PostgresJsDatabase } from "drizzle-orm/postgres-js"
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle"
 import postgres from "postgres"
 import { drizzle } from "drizzle-orm/postgres-js"
 import * as schema from "@/data/schema.ts"
 
 const pg: ReturnType<typeof postgres> = postgres(import.meta.env.DATABASE_URL)
 const database: PostgresJsDatabase<typeof schema> = drizzle(pg, { schema })
+const lucid = new DrizzlePostgreSQLAdapter(database, schema.luciaSessionTable, schema.luciaUsersTable)
 
-export { database, pg }
+export { pg, database, lucid }
--- src/data/schema.ts
+++ src/data/schema.ts
@@ -1,7 +1,19 @@
-import { pgTable, varchar } from "drizzle-orm/pg-core";
+import { pgTable, varchar, text, timestamp } from "drizzle-orm/pg-core";
 import { nanoid } from "nanoid"
 
 export const xdumps = pgTable("xdumps", {
     id: varchar("id").primaryKey().$defaultFn(() => nanoid()),
     txt: varchar("txt")
+})
+
+export const luciaUsersTable = pgTable("lucia_users", {
+    id: text("id").primaryKey(),
+    name: text("name").notNull(),
+    email: text("email")
+})
+
+export const luciaSessionTable = pgTable("lucia_sessions", {
+    id: text("id").primaryKey(),
+    userId: text("user_id").notNull().references(() => luciaUsersTable.id),
+    expiresAt: timestamp("expires_at", { withTimezone: true, mode: "date" }).notNull()
 })
