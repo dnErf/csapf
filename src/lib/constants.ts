6f857d450e5a0f10be86bbc4935a5ce55cb35f56
ff <frend.fernando@LIVE.COM>
2024-11-07 15:03:18 -0800

    fx directus url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
+export const DIRECTUS_URL = "https://directus.vdbx.duckdns.org"
 export const TRAVERSY_SITE = "Tech Blog"
 export const TRAVERSY_SITE_DESCRIPTION = "Articles, Stories and Tutorials"
 export const TRAVERSY_ARTICLE_LIMIT = 6
